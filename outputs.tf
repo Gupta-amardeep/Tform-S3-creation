c4c341f4ca68ba3265363d023a17e0673b5e7a4a
Amardeep Gupta <48344645+Gupta-amardeep@users.noreply.github.com>
2021-01-31 13:36:48 +0530

    updating outputs file

--- outputs.tf
+++ outputs.tf
@@ -2,16 +2,8 @@ output "s3_bucket_id" {
   value = aws_s3_bucket.bucket.*.id
 }
 
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.bucket.*.arn
-}
-
 output "s3_bucket_domain_name" {
   value = aws_s3_bucket.bucket.*.bucket_domain_name
-}
-
-output "s3_hosted_zone_id" {
-  value = aws_s3_bucket.bucket.*.hosted_zone_id
 }
 
 output "s3_bucket_region" {
